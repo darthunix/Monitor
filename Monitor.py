f2a37103f0d7db2c3a2baffc5857b7cf9ba2d230
darthunix <darthunix@gmail.com>
2016-11-10 16:37:47 +1000

    Добавил логирование

--- Monitor.py
+++ Monitor.py
@@ -1,13 +1,13 @@
 import os
 import asyncio
 import argparse
+import logging
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 
 class MountPoint:
-
     def __init__(self, path):
         self.path = path
 
@@ -17,7 +17,6 @@ class MountPoint:
 
 
 class Size:
-
     def __init__(self, byte_size):
         self.byte_size = byte_size
         self.base = 1024
@@ -39,19 +38,22 @@ class Size:
 
 
 class Email:
-
     def __init__(self, from_mail, to_mail):
         self.from_mail = from_mail
         self.to_mail = to_mail
+
+    @staticmethod
+    def body(mount_point, free_size):
+        return "На разделе '{mount_point}' компьютера '{hostname}' заканчивается свободное место. " \
+               "На текущий момент свободно {free_size} ГБ." \
+            .format(mount_point=mount_point, hostname=os.uname().nodename, free_size=free_size)
 
     def message(self, mount_point, free_size):
         msg = MIMEMultipart()
         msg['Subject'] = "Внимание! Заканчивается свободное место на разделе!"
         msg['From'] = self.from_mail
         msg['To'] = self.to_mail
-        text = "На разделе '{mount_point}' компьютера '{hostname}' заканчивается свободное место. " \
-               "На текущий момент свободно {free_size} ГБ." \
-            .format(mount_point=mount_point, hostname=os.uname().nodename, free_size=free_size)
+        text = self.body(mount_point, free_size)
         msg.attach(MIMEText(text, 'plain'))
         return msg
 
@@ -70,10 +72,18 @@ if __name__ == "__main__":
     parser.add_argument("to_mail", nargs="?", default="smirnov@viveya.khv.ru",
                         help="Почтовый адрес, на который будут приходить предупреждения.")
     parser.add_argument("smtp", nargs="?", default="mail.viveya.khv.ru", help="Почтовый сервер SMTP.")
+    parser.add_argument("log_file", nargs="?", default="/tmp/monitor.log", help="Лог файл скрипта.")
+    parser.add_argument("log_level", nargs="?", default=logging.INFO, help="Уровень логирования скрипта.")
     options = parser.parse_args()
 
     if not os.path.exists(options.mount_point):
-        raise Exception("Данная точка монтирования не существует!")
+        error_message = "Данная точка монтирования не существует!"
+        logging.error(error_message)
+        raise Exception(error_message)
+
+    logging.basicConfig(filename=options.log_file, level=options.log_level,
+                        format="%(levelname)-8s [%(asctime)s]  %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+
 
     def monitoring(ioloop):
         mount_point = MountPoint(options.mount_point)
@@ -82,6 +92,12 @@ if __name__ == "__main__":
             with smtplib.SMTP(options.smtp) as smtp:
                 smtp.sendmail(email.from_mail, email.to_mail,
                               email.message(options.mount_point, mount_point.free_size().gb()).as_string())
+            logging.warning("Отправлено письмо на {to_mail} с содержанием: '{message}'"
+                            .format(to_mail=options.to_mail,
+                                    message=email.body(options.mount_point, mount_point.free_size().gb())))
+        else:
+            logging.debug("Все хорошо. На '{mount_point}' свободно еще {free_size} ГБ."
+                         .format(mount_point=options.mount_point, free_size=options.free_size))
         ioloop.call_later(options.sleep_interval, monitoring, ioloop)
 
     loop = asyncio.get_event_loop()
@@ -90,3 +106,4 @@ if __name__ == "__main__":
         loop.run_forever()
     finally:
         loop.close()
+        logging.info("Программа завершена")
